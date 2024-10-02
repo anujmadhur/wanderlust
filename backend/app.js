7eac4c2c7b04501c5a884078397d569fc283f598
Chaitany233patil <patilchaitany529@gmail.com>
2024-10-02 17:13:37 +0530

    Initial commit with project files

---
+++ backend/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const Listings = require("./models/listing");
+const mongoose = require("mongoose");
+const { log } = require("console");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+const port = 8080;
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/PropertyHubDB";
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("hello i am root");
+});
+
+app.get("/listings", async (req, res) => {
+  let listing = await Listings.find();
+  res.render("index.ejs", { listings: listing });
+});
+
+app.get("/listings/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/listings/add", async (req, res) => {
+  let newListing = new Listings(req.body.listing);
+  newListing.image = { url: req.body.Imageurl, filename: "listingImage" };
+  newListing.owner = "66f1703f56642d599967f39e";
+  console.log(newListing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listings.findById(id);
+  res.render("show.ejs", { listing });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
